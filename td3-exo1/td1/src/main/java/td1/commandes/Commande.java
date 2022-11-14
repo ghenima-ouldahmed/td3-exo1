68e976088e0aa25e59847dc4ea93e9601517e37c
ghenima-ouldahmed <112975120+ghenima-ouldahmed@users.noreply.github.com>
2022-11-14 17:27:40 +0100

    Add files via upload

---
+++ td3-exo1/td1/src/main/java/td1/commandes/Commande.java
@@ -0,0 +1,87 @@
+package td1.commandes;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import td1.paires.Paire;
+
+public class Commande<T> {
+    private List<Paire<Produit, Integer>> lignes;
+
+    public Commande() {
+        this.lignes = new ArrayList<>();
+    }
+
+    public Commande ajouter(Produit p, int q) {
+        lignes.add(new Paire<>(p, q));
+        return this;
+    }
+
+    public List<Paire<Produit, Integer>> lignes() {
+        return lignes;
+    }
+    // réécriture de la méthode toString en utilisant formatteurLigne, map et collect
+    @Override
+    public String toString() {
+        return lignes.stream().map(l -> formateurLigne.apply(l)).collect(Collectors.joining());
+    } 
+
+
+    //définition de la fonction formatteurLigne 
+    private static final Function<Paire<Produit, Integer>, String> formateurLigne = l-> String.format("%s %d",  l.fst(), l.snd());
+    
+    
+    /**
+     * cumule les lignes en fonction des produits
+     */
+
+
+    public Commande normaliser() {
+        Map<Produit, Integer> lignesCumulees = new HashMap<>();
+        for (Paire<Produit, Integer> ligne : lignes) {
+            Produit p = ligne.fst();
+            int qte = ligne.snd();
+            if (lignesCumulees.containsKey(ligne.fst())) {
+                lignesCumulees.put(p, lignesCumulees.get(p) + qte);
+            } else {
+                lignesCumulees.put(p, qte);
+            }
+        }
+        Commande commandeNormalisee = new Commande();
+        for (Produit p : lignesCumulees.keySet()) {
+            commandeNormalisee.ajouter(p, lignesCumulees.get(p));
+        }
+        return commandeNormalisee;
+    }
+    // Réécriture de la méthode cout avec map et reduce
+    public Double cout(Function<Paire<Produit, Integer>, Double> calculLigne) {
+        return lignes.stream().map(l -> calculLigne.apply(l)).reduce(0.0, (a,b) -> a + b) ;
+    }
+
+    
+    public String affiche(Function<Paire<Produit, Integer>, Double> calculLigne) {
+        Commande c = this.normaliser();
+        final String HLINE = "+------------+------------+-----+------------+--------+------------+\n";
+        StringBuilder str = new StringBuilder();
+        str.append("\n\nCommande\n");
+        str.append(HLINE);
+        str.append("+ nom        + prix       + qté + prix ht    + tva    + prix ttc   +\n");
+        str.append(HLINE);
+        for (Paire<Produit, Integer> ligne : c.lignes) {
+            str.append(String.format("+ %10s + %10.2f + %3d + %10.2f + %5.2f%% + %10.2f +\n", ligne.fst(), // nom
+                    ligne.fst().prix(), // prix unitaire
+                    ligne.snd(), // qté
+                    ligne.fst().prix() * ligne.snd(), // prix ht
+                    ligne.fst().cat().tva() * 100, // tva
+                    calculLigne.apply(ligne)));
+        }
+        str.append(HLINE);
+        str.append(String.format("Total : %10.2f", c.cout(calculLigne)));
+        return str.toString();
+    }
+
+}
